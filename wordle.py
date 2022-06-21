5b74f066550ed2f909b6c7fa67589b42c9575f04
Omar Macias <a01635795@tec.mx>
2022-06-21 13:28:28 -0500

    Functional system

---
+++ wordle.py
@@ -0,0 +1,31 @@
+def won(tried):
+    return set(tried) == {"🟩"}
+
+
+def change_input_visual(lst_guess, tried):
+    lst_tried = list(tried)
+    lst_vis = list(tried)
+    lst_letters = lst_guess[::]
+    for i in range(len(lst_guess)):
+        if lst_tried[i] == lst_guess[i]:
+            lst_vis[i] = "🟩"
+            lst_letters.remove(lst_tried[i])
+    for i in range(len(lst_guess)):
+        if lst_tried[i] in lst_guess and lst_tried[i] in lst_letters\
+        and lst_vis[i] != "🟩":
+            lst_vis[i] = "🟨"
+            lst_letters.remove(lst_tried[i])
+    return "".join(lst_vis)
+
+
+win = False
+word_to_guess = "PERRO"
+lst_word = list(word_to_guess)
+while win == False:
+    visual = "_ "*len(word_to_guess)
+    inp = input(f"Try your guess of: \n{visual}\n").upper()
+    inp = change_input_visual(lst_word, inp)
+    print(inp)
+
+    win = won(inp)
+print("Ganaste")
