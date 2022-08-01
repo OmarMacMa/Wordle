04e6557bcedfe82798bf7b6c91ddcb9e9a174ffb
Omar Macias <a01635795@tec.mx>
2022-08-01 17:29:23 -0500

    Static Syntax

--- wordle.py
+++ wordle.py
@@ -1,11 +1,12 @@
 from random import randint
+from typing import List
 
 
-def won(tried):
+def won(tried: str) -> bool:
     return set(tried) == {"🟩"}
 
 
-def change_input_visual(lst_guess, tried):
+def change_input_visual(lst_guess: List, tried: str) -> str:
     lst_tried = list(tried)
     lst_vis = list(tried)
     lst_letters = lst_guess[::]
@@ -21,24 +22,28 @@ def change_input_visual(lst_guess, tried):
     return "".join(lst_vis)
 
 
-def select_random_word():
+def select_random_word() -> str:
     fl = open("words_bank.txt", "r")
     rand = randint(0, 19)
     words = fl.readlines()
     fl.close()
     return words[rand].rstrip()
 
+def main():
+    win: bool = False
+    word_to_guess: str = select_random_word()
+    lst_word: List = list(word_to_guess)
+    while win == False:
+        visual: str = "_ "*len(word_to_guess)
+        inp: str = input(f"Try your guess of: \n{visual}\n").upper()
+        while len(inp) != len(word_to_guess):
+            print("The guess must be of the same length")
+            inp = input(f"Try your guess of: \n{visual}\n").upper()
+        inp = change_input_visual(lst_word, inp)
+        print(inp)
+        win = won(inp)
+    print("You won")
 
-win = False
-word_to_guess = select_random_word()
-lst_word = list(word_to_guess)
-while win == False:
-    visual = "_ "*len(word_to_guess)
-    inp = input(f"Try your guess of: \n{visual}\n").upper()
-    while len(inp) != len(word_to_guess):
-        print("The guess must be of the same length")
-        inp = input(f"Try your guess of: \n{visual}\n").upper()
-    inp = change_input_visual(lst_word, inp)
-    print(inp)
-    win = won(inp)
-print("You won")
+
+if __name__ == "__main__":
+    main()
